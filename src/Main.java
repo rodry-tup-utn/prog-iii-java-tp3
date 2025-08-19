f3742378527a132c1e01cce1edb8d55b55e56bb7
rodrymza <rodrymza@gmail.com>
2025-08-18 21:24:24 -0300

    Finalizacion de TP 3

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,36 @@
+import javax.swing.plaf.ProgressBarUI;
+import java.util.ArrayList;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
+        Producto producto1 = new Producto("Laptop Dell XPS 15", "DELL-XPS15", 1500.00);
+        Producto producto2 = new Producto("Mouse Logitech G502", "LOG-G502", 79.99);
+        Producto producto3 = new Producto("Teclado Mecánico Razer", "RAZ-MECH-KB", 120.50);
+        Producto producto4 = new Producto("Monitor Samsung Ultrawide", "SAM-UW-34", 450.75);
+        Producto producto5 = new Producto("Auriculares Sony WH-1000XM4", "SONY-XM4", 249.00);
 
+        ArrayList<Producto> listaProductos = new ArrayList<Producto>();
+
+        //Agregar productos a la lista:
+        agregarProductoLista(listaProductos, producto1);
+        agregarProductoLista(listaProductos, producto1);    //Intentar agregarlo nuevamente
+        agregarProductoLista(listaProductos, producto2);
+        agregarProductoLista(listaProductos, producto3);
+        agregarProductoLista(listaProductos, producto3);    //Intentar agregarlo nuevamente
+        agregarProductoLista(listaProductos, producto4);
+        agregarProductoLista(listaProductos, producto5);
+
+    }
+
+    public static void agregarProductoLista(ArrayList<Producto> listaProductos, Producto producto) {
+        if (listaProductos.contains(producto)) {
+            System.out.println("*** El producto " + producto.getNombre() + " ya existe ***");
+        } else {
+            listaProductos.add(producto);
+            System.out.println("Producto " + producto.getNombre() + " agregado");
+        }
     }
 }
---
+++ src/Producto.java
@@ -0,0 +1,49 @@
+import java.util.Objects;
+
+public class Producto {
+    private String nombre;
+    private String codigo;
+    private double precio;
+
+    public Producto(String nombre, String codigo, double precio) {
+        this.nombre = nombre;
+        this.codigo = codigo;
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Producto producto = (Producto) o;
+        return Objects.equals(getCodigo(), producto.getCodigo());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getCodigo());
+    }
+}
